61bf295c1c2e40efd437fa35b5290ffd7ceaabe5
hao-nanxiong <hao_nanxiong@163.com>
2023-02-18 16:08:06 +0800

    完成了navigation的搭建与页面跳转

--- app/src/main/java/com/example/navigationdemo/MainActivity.java
+++ app/src/main/java/com/example/navigationdemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.navigationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //创建NavController
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        return navController.navigateUp();
     }
 }
