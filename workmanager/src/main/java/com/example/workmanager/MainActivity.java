f35c52c8d11fad83b1ba73106b0f37bbdf820517
hao-nanxiong <hao_nanxiong@163.com>
2023-02-19 11:04:37 +0800

    定义任务与执行结果,设置任务触发条件和延迟执行任务

--- workmanager/src/main/java/com/example/workmanager/MainActivity.java
+++ workmanager/src/main/java/com/example/workmanager/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.workmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +22,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void mAddWork(View view) {
 
+        //添加约束,需要网络的时候会执行
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+                .build();
+
+        //配置我们的任务
+        //一次性执行的任务
+        OneTimeWorkRequest workRequest = new OneTimeWorkRequest
+                //设置触发条件,定义一些约束
+                .Builder(MyWork.class)
+                .setConstraints(constraints)
+                .setInitialDelay(5, TimeUnit.SECONDS).build();
+        //任务交给WorkManager队列里
+        WorkManager workManager = WorkManager.getInstance(this);
+        workManager.enqueue(workRequest);
     }
 }
--- workmanager/src/main/java/com/example/workmanager/MyWork.java
+++ workmanager/src/main/java/com/example/workmanager/MyWork.java
@@ -1,4 +1,26 @@
 package com.example.workmanager;
 
-public class MyWork {
+import android.content.Context;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWork extends Worker {
+
+
+    public MyWork(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    //定义我们的任务
+    @NonNull
+    @Override
+    public Result doWork() {
+        SystemClock.sleep(2000);
+        Log.d("hu","MyWork");
+        return Result.success();
+    }
 }
